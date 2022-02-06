b95509ed020f566c9bccf67675d5c10c39996bb8
G0x209C <g@collette-online.nl>
2022-02-06 03:28:01 +0100

    :package: Initial commit

---
+++ vetur.config.js
@@ -0,0 +1,9 @@
+module.exports={
+    settings:{
+        "vetur.useWorkspaceDependencies": true,
+        "veture.experimental.templateInterpolationService": true
+    },
+
+    projects:['./frontend']
+
+}
