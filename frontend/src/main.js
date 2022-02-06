a4fbd488a4f14f6a261958e82a12724e3b7a1ea7
G0x209C <g@collette-online.nl>
2022-02-06 17:22:20 +0100

    :package: GlobalCookiesConfig

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import io from '@/socket';
+import {globalCookiesConfig} from "vue3-cookies";
 
 const app = createApp(App).use(store).use(router)
 
+globalCookiesConfig({
+    expireTimes: "15m",
+    path: '/',
+    domain: 'localhost',
+    secure: false,
+    sameSite: "Lax",
+});
 
 app.config.globalProperties.$io = io;
 
